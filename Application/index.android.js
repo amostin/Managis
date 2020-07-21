8476a230a1051b98572ffdcff2177e7e3b995ffc
amostin <a.mostin@students.ephec.be>
2020-07-22 00:39:58 +0200

    suppression fichier inutile

--- Application/index.android.js
+++
@@ -1,5 +0,0 @@
-import React, { Component } from 'react';
-import { AppRegistry,View,Text,StyleSheet } from 'react-native';
-
-import ManagisApp from './app/app.js';
-AppRegistry.registerComponent('ManagisApp', () => ManagisApp);
