89247949ca20795a4c622582f6111253efefe05c
amostin <a.mostin@students.ephec.be>
2020-07-22 22:52:09 +0200

    bouton inscription

--- appManagis/components/Inscription.js
+++ appManagis/components/Inscription.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, SafeAreaView, View } from "react-native";
 
 class Inscription extends React.Component {
     constructor(props) {
@@ -8,10 +8,48 @@ class Inscription extends React.Component {
 
     render() {
         return (
-            <Text>Inscription</Text>
+            <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                
+                <View style={styles.submitContainer}>
+                    <TouchableOpacity
+                        onPress={() => this.props.navigation.navigate('Inscription')}>
+                        <Text style={styles.submitButton}>Inscription</Text>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
         )
     }
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignContent: 'center',
+    },
+    submitButton: {
+        width: 100,
+        borderRadius: 25,
+        paddingVertical: 13,
+        textAlign: 'center',
+        color: '#FFFFFF',
+
+    },
+    submitContainer: {
+        backgroundColor: "#6D071A",
+        borderRadius: 25,
+        marginVertical: 10,
+    },
+    imgBack: {
+        width: '100%',
+        height: '80%',
+        resizeMode: 'repeat',
+        justifyContent: 'center',
+        alignItems: 'center',
+        right: 20,
+        top: 120,
+        opacity: 0.2,
+        position: 'absolute',
+    }
+});
 
 export default Inscription
