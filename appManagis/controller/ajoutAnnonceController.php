31eb624b4be620438fbb1401fd9eea25defe09ad
amostin <a.mostin@students.ephec.be>
2020-07-26 12:25:53 +0200

    no xss

--- appManagis/controller/ajoutAnnonceController.php
+++ appManagis/controller/ajoutAnnonceController.php
@@ -4,11 +4,11 @@ include 'dbAccess.php';
 $db = new dbAccess();
 $json = file_get_contents('php://input');
 $obj = json_decode($json,true);
-$user = $obj['userId']; //Met l id de user connecte que tu as dans ton JS en JSON
-$nomReste = $obj['nomReste'];
-$quantiteReste =  $obj['quantiteReste'];
-$descriptionReste = $obj['descriptionReste'];
-$adresse = $obj['adresse'];
+$user = htmlspecialchars($obj['userId']); //Met l id de user connecte que tu as dans ton JS en JSON
+$nomReste = htmlspecialchars($obj['nomReste']);
+$quantiteReste =  htmlspecialchars($obj['quantiteReste']);
+$descriptionReste = htmlspecialchars($obj['descriptionReste']);
+$adresse = htmlspecialchars($obj['adresse']);
 $ajoutAnnonce = $db->procCall('ajoutAnnonce', [$user,$nomReste, $quantiteReste, $descriptionReste, $adresse]);
 //Faites un foreach ou quoi pour pas avoir de problèmes ... je le fais au cas ou
 echo json_encode($ajoutAnnonce); //Decode le en JS
